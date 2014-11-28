3872e8761bbf08081c75e6fc6dba5d0dd3f7661e
liu.huidong <liuhd@mycomm.cn>
2014-11-28 09:05:40 +0800

    启发式搜索

---
+++ src/heuristicsearch/best_fs.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%%% @author lhd
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 27. Nov 2014 11:45 PM
+%%%-------------------------------------------------------------------
+-module(best_fs).
+-author("lhd").
+
+%% API
+-export([best_fs/2]).
+
+best_fs(Open_pqueue, Close_pqueue)
